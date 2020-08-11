7470e586d1b5e670b566a7097d6c8b8452707efa
CyberLoli <45149264+cyber-loli@users.noreply.github.com>
2020-08-11 17:27:30 +0700

    Add files via upload

--- BssidFinder.py
+++ BssidFinder.py
@@ -1,28 +1,28 @@
 import requests
 import xmltodict
 
-mac = input("Enter BSSID here: ").replace(':', '')
+mac = input("Enter BSSID here: ")
+
+def FindFromBssid(bssid):
+
+    bssid = bssid.replace(':', '')
+
+    response = requests.get('http://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks='+bssid+':-65&app=ymetro')
+
+    response.encoding = 'utf-8'
+
+    dicto = dict(xmltodict.parse(response.text))
+    dcordos = dict(dict(dicto['location'])['coordinates'])
+
+    return {'latitude': float(dcordos['@latitude']), 'longitude': float(dcordos['@longitude'])}
 
 try:
-    response = requests.get('http://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks='+mac+':-65&app=ymetro')
+    result = FindFromBssid(mac)
+except KeyError:
+    print("Bssid location not found.")
+    exit()
 except Exception:
     print('Unable to connect to the internet.')
     exit()
 
-response.encoding = 'utf-8'
-
-dicto = dict(xmltodict.parse(response.text))
-
-try:
-    dcordos = dict(dict(dicto['location'])['coordinates'])
-except KeyError:
-    print("Bssid location not found.")
-    exit()
-
-print("""
-Latitude: {0}
-Longitude: {1}
-
-Nlatitude: {2}
-Nlongitude: {3}
-""".format(dcordos['@latitude'], dcordos['@longitude'], dcordos['@nlatitude'], dcordos['@nlongitude']))
+print('Latitude: {0}\nLongitude: {1}'.format(result['latitude'], result['longitude']))
